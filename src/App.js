1bec683180ed575976e21eea101c7da4b194127e
farhanmaneri <farhanmaneri@gmail.com>
2023-08-28 10:34:30 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MenuPage from './pages/MenuPage'
+
+export default function App() {
+  return (
+    <>
+    <MenuPage />
+    </>
+  )
+}
---
+++ src/components/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import KfcCard from './KfcCard';
+import { items } from '../data/prodcutsData';
+
+export default function Products({increment}) {
+  return (
+    <div className="row row-cols-1 row-cols-md-4 g-4">
+        
+    {
+        items.map(item => <KfcCard key={item.id} item={item} increment={increment}/>)
+    }
+    </div>
+  )
+}
